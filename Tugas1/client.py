53044a646f531ecf9562248d8e1f4ca6332cb351
Muhammad Ivan Riansyah Putra <33018078+shakellzero@users.noreply.github.com>
2019-02-26 17:32:49 +0800

    Update client.py

--- Tugas1/client.py
+++ Tugas1/client.py
@@ -5,14 +5,13 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = ('127.0.0.1', 8000)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
-
 try:
     # Send data
-    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+    message = 'Ini adalah pesan yang terkirim'
     print >>sys.stderr, 'sending "%s"' % message
     sock.sendall(message)
     # Look for the response
